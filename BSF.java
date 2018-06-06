6bf7559d474a91c70ecbba9c02be06561def0f99
Julio <jfuenmayor96@users.noreply.github.com>
2018-06-06 11:55:05 -0400

    semi working example

--- BSF.java
+++ BSF.java
@@ -24,4 +24,64 @@ import org.apache.hadoop.io.Text;
 
 public class BFS extends Configured implements Tool {
     
+  private static final Logger LOG = Logger.getLogger(BFS.class);
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new BFS(), args);
+    System.exit(res);
+  }
+
+  /*
+    Map class
+
+    Parameters:
+        @param IntWritable: Input key type
+        @param IntWritable: Input value type
+        @param IntWritable: Output key type
+        @param IntWritable: Output value type
+   */
+  public static class Map extends Mapper<LongWritable, Text, LongWritable, Text> {
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+      	  String line = value.toString();
+          String[] fields = line.split("\t");
+          context.write(new LongWritable(Integer.parseInt(fields[0])), new Text(fields[1]));
+        }
+  }
+
+  /*
+    Reduce class
+
+    Parameters:
+        @param IntWritable: Input key type
+        @param IntWritable: Input value type
+        @param IntWritable: Output key type
+        @param IntWritable: Output value type
+   */
+  public static class Reduce extends Reducer<LongWritable, Text, LongWritable, Text> {
+    @Override
+    public void reduce(LongWritable key, Iterable<Text> values, Context context)
+        throws IOException, InterruptedException {
+	    System.out.println(values);
+	    
+
+            for (Text value : values) {
+              System.out.println(value);
+	      
+	      context.write(new LongWritable(Integer.parseInt(value.get())), new Text(value));
+            }
+        }
+  }
+
+    public int run(String[] args) throws Exception {
+      Job job = Job.getInstance(getConf(), "BFS");
+      job.setJarByClass(this.getClass());	
+      FileInputFormat.addInputPath(job, new Path(args[0]));
+      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+      job.setMapperClass(Map.class);
+      job.setReducerClass(Reduce.class);
+      job.setOutputKeyClass(LongWritable.class);
+      job.setOutputValueClass(Text.class);
+      return job.waitForCompletion(true) ? 0 : 1;
+    }
 }
